94edaf33616d2c50e1844cdfb714aa5dfb582599
wacdev <i@wac.tax>
2023-03-29 16:47:17 +0800

    ♨

--- conf/dev.js
+++ conf/dev.js
@@ -2,5 +2,5 @@ import lang from "wac.tax/_/lang.js";
 
 export const USER_TAX_CDN = "//i18n.wac.tax/",
 	I18N_CDN = () => "/i18n/" + lang(),
-	API = "https://api.wac.tax/";
-// API = "//127.0.0.1:8880/";
+	API = "//127.0.0.1:8880/";
+// API = "https://api.wac.tax/";
